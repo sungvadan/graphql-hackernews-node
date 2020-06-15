efeda24a2851965065203e03262b629724e47dbb
sungvadan <svd.phan@gmail.com>
2020-06-15 22:44:01 +0200

    fixup! fixup! graphql

--- src/index.js
+++ src/index.js
@@ -8,32 +8,58 @@ let links = [{
 
 let idCount = links.length
 
+const findLinkById = (id) => {
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].id === id) {
+            return i
+        }
+    }
+
+    return -1;
+}
 const resolvers = {
     Query: {
         info: () => `This is the API of a hackernew Clone`,
         feed: () => links,
         link: (parent, args) => {
-            let link = null;
-            for (let i = 0; i < links.length; i++) {
-                if (links[i].id === args.id) {
-                    link = links[i]
-                }
+            let index = findLinkById(args.id)
+            if (index !== -1) {
+                return links[index]
             }
-
-            return link
+            return null;
         }
     },
     Mutation: {
-      post: (parent, args) => {
-          let link = {
-              id: `link-${idCount}`,
-              description: args.description,
-              url: args.url,
-          }
-          links.push(link)
+        post: (parent, args) => {
+              let link = {
+                  id: `link-${idCount}`,
+                  description: args.description,
+                  url: args.url,
+              }
+              links.push(link)
 
-          return link
-      }
+              return link
+        },
+        updateLink: (parent, args) => {
+            let link = null
+            let index = findLinkById(args.id)
+            if (index !== -1) {
+                link = links[index]
+                link.url = args.url
+                link.description = args.description
+                links = Object.assign([], links, {index: link})
+            }
+            return link
+        },
+        deleteLink: (parent, args) => {
+            let index = findLinkById(args.id)
+            let link = null
+            if (index !== -1) {
+                link = links[index]
+                links.splice(index, 1)
+            }
+            return link
+        }
     },
 }
 
